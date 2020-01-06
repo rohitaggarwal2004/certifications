92c1c6cd6261c7efa9af12f06a9008073a371156
rohit aggarwal <rohit.aggarwal@rohitaggarwal.net>
2020-01-06 19:04:10 +0530

    Added Sleuth

--- certifications/src/main/java/com/certifications/controller/CertificationController.java
+++ certifications/src/main/java/com/certifications/controller/CertificationController.java
@@ -3,15 +3,20 @@ package com.certifications.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.hibernate.validator.internal.util.logging.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.certifications.model.Certificate;
 
+
+
 @RestController
 @RequestMapping(path = "/certificates")
 public class CertificationController {
+	Logger logger = org.slf4j.LoggerFactory.getILoggerFactory().getLogger(this.getClass().getName());
 
 	@GetMapping(path = "/list")
 	public List<Certificate> getCertifications() {
@@ -21,6 +26,11 @@ public class CertificationController {
 				new Certificate("AWS Certified Associate  Developer", "AWS DA", "", "2018"),
 				new Certificate("Java EJB Certified", "SCBCD", "", ""),
 				new Certificate("Sun Java Certified Develper (java 6)", "SCP", "", ""));
+		for(Certificate certificate :list)
+		{
+			logger.info("Rohit Aggarwal " + certificate.toString());
+			
+		}
 		return list;
 
 	}
