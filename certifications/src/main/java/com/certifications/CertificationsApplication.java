2694f57032216ce75c62285c44ca636f027c4851
roaggarw <roaggarw@dhcp-10-17-56-92.in.oracle.com>
2020-02-11 12:33:48 +0530

    thx

--- certifications/src/main/java/com/certifications/CertificationsApplication.java
+++ certifications/src/main/java/com/certifications/CertificationsApplication.java
@@ -2,22 +2,18 @@ package com.certifications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-
-import brave.sampler.Sampler;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class CertificationsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CertificationsApplication.class, args);
 	}
-	@Bean
-	public Sampler defaultSampler()
-	{
-		 return Sampler.ALWAYS_SAMPLE;
-		
-	}
+//	@Bean
+//	public Sampler defaultSampler()
+//	{
+//		 return Sampler.ALWAYS_SAMPLE;
+//		
+	//}
 }
--- certifications/src/main/java/com/certifications/controller/CertificationController.java
+++ certifications/src/main/java/com/certifications/controller/CertificationController.java
@@ -1,17 +1,15 @@
 package com.certifications.controller;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
-import org.hibernate.validator.internal.util.logging.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.certifications.model.Certificate;
-
-
+import com.certifications.model.Certification;
 
 @RestController
 @RequestMapping(path = "/certificates")
@@ -19,17 +17,16 @@ public class CertificationController {
 	Logger logger = org.slf4j.LoggerFactory.getILoggerFactory().getLogger(this.getClass().getName());
 
 	@GetMapping(path = "/list")
-	public List<Certificate> getCertifications() {
-		List<Certificate> list = Arrays.asList(
-				new Certificate("Google Certified Profressional Architect", "GCP", "", "2019"),
-				new Certificate("AWS Certified Associate  Architect", "AWS CAA", "", "2018"),
-				new Certificate("AWS Certified Associate  Developer", "AWS DA", "", "2018"),
-				new Certificate("Java EJB Certified", "SCBCD", "", ""),
-				new Certificate("Sun Java Certified Develper (java 6)", "SCP", "", ""));
-		for(Certificate certificate :list)
-		{
+	public List<Certification> getCertifications() {
+		List<Certification> list = Arrays.asList(
+				new Certification("Google Certified Profressional Architect", "GCP", "", "2019"),
+				new Certification("AWS Certified Associate  Architect", "AWS CAA", "", "2018"),
+				new Certification("AWS Certified Associate  Developer", "AWS DA", "", "2018"),
+				new Certification("Java EJB Certified", "SCBCD", "", ""),
+				new Certification("Sun Java Certified Develper (java 6)", "SCP", "", ""));
+		for (Iterator<Certification> iterator = list.iterator(); iterator.hasNext();) {
+			Certification certificate = iterator.next();
 			logger.info("Rohit Aggarwal " + certificate.toString());
-			
 		}
 		return list;
 
